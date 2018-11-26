df1aac73af4e5618f7f052c67eff279cd3e488a8
sbuteryd@qq.com <sbuteryd@qq.com>
2018-11-26 14:40:58 +0800

    feat:add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,17 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
     return gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+
         .pipe(gulp.dest('./css'));
 });
